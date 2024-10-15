7cff5d9efb56ecf95a30d43bb69bd082d47f8cb7
ngxdjt <tyson.huynh@laetottenham.ac.uk>
2024-10-15 08:39:23 +0000

    Pending changes exported from your codespace

--- currency_exchange_tool.py
+++ currency_exchange_tool.py
@@ -1,13 +1,24 @@
-import math # you'll probably need this
+import math
 
 exchange_rates = {
-    'USD': 1.13, #I.E. 1 Pound is 1.13 Dollars
+    'USD': 1.13, 
     'EUR': 1.15,
+    'VND': 32442.36,
+    'ZAR': 22.87,
+    'CAD': 1.80
 }
 
 def check_currency_exists(currency):
-    return
+    if currency in exchange_rates:
+        return True
+    else:
+        return False
 
 def currency_convert(original_c, new_c, amount):
-    # your code here
-    return
+    if check_currency_exists(new_c) and original_c == "GBP":
+        amount *= exchange_rates[new_c]
+        return amount 
+    elif not check_currency_exists(new_c):
+        return ("Currency not avaliable in store")
+    elif original_c != "GBP":
+        return ("Cannot convert from that currency")
