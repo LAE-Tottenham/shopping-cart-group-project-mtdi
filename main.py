a4ce9cb06ebdcd7eb1e839f4a04518a75f45310f
inas-raka <inas.raka@laetottenham.ac.uk>
2024-10-17 11:07:34 +0000

    Pending changes exported from your codespace

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from currency_exchange_tool import currency_convert
 from shop_functions import start_shop
+import pyfiglet
 
-print('Welcome to my shop')
+print(pyfiglet.figlet_format('Welcome to my shop', font = "term"))
 
-while True:
-    print('Please select what you would like to buy')
-    items_to_buy = start_shop()
 
-    # blah blah 
+print('Please select what you would like to buy')
+items_to_buy = start_shop()
