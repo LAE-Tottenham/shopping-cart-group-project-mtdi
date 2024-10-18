1b4d4313c7dd632d99e663c7d8ce91c4a0822d0f
David777101 <David.Isaac-Ojo@laetottenham.ac.uk>
2024-10-18 12:20:58 +0100

    cart.py

--- cart.py
+++ cart.py
@@ -1,4 +1,26 @@
+
 import pyfiglet
+
+#account creation
+print("Create account")
+email=input("Please enter your email address: ")
+password=input("Please enter a secure password: ")
+
+
+#account login
+print("Login to your account")
+entered_email=input("Please enter your email address: ")
+entered_password=input("Please enter your password: ")
+
+
+#login details check
+while entered_email!=email or entered_password!=password:
+  print("Wrong email address or password. Try again")
+  entered_email=input("Please enter your email address: ")
+  entered_password=input("Please enter your password: ")
+else:
+  print("Welcome")
+  
 
 def getContactDetails():
     print(pyfiglet.figlet_format("CONTACT DETAILS", font = "straight" ))
@@ -33,3 +55,27 @@ card_no = int(input(pyfiglet.figlet_format("Card number: ", font = "straight")))
 expiry_date = input(pyfiglet.figlet_format("Enter expiry date in the format MM/YY: ", font = "straight"))
 name = input(pyfiglet.figlet_format("Enter name: ", font = "straight"))
 cvv = int(input(pyfiglet.figlet_format("Enter cvv: ", font = "straight")))
+
+f = open("account_file.txt", "w")
+f.write(pyfiglet.figlet_format("CUSTOMER DETAILS" + '\n'))
+f.close()
+
+f = open("account_file.txt", "a")
+
+f.write(str("") + '\n')
+f.write(pyfiglet.figlet_format("Name: " + str(name.upper()) + '\n'))
+f.write(pyfiglet.figlet_format(str(email.upper()) + '\n'))
+f.write(pyfiglet.figlet_format(str(password.upper()) + '\n'))
+f.write(pyfiglet.figlet_format(str(payment_method.upper()) + '\n'))
+f.write(pyfiglet.figlet_format(str(card_no) + '\n'))
+f.write(pyfiglet.figlet_format(str(expiry_date) + '\n'))
+f.write(pyfiglet.figlet_format(str(cvv) + '\n'))
+f.write(pyfiglet.figlet_format(str(address.upper()) + '\n'))
+f.write(pyfiglet.figlet_format(str(country.upper) + '\n'))
+f.write(pyfiglet.figlet_format(str(town_city.upper()) + '\n'))
+f.write(pyfiglet.figlet_format(str(postcode.upper()) + '\n'))
+
+f.close()
+
+f = open("account_file.txt", "r")
+print(f.read())
