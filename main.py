c35dec5043dbb373cb822a586dc2eea396f26591
Maxrampling794 <max.rampling@laetottenham.ac.uk>
2024-11-04 12:28:01 +0000

    Pending changes exported from your codespace

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
-from currency_exchange_tool import currency_convert
+
+import pyfiglet as pfg 
+from currency_exchange_tool import *  
 from shop_functions import start_shop
+from account import *
+from cart import *
+login_or_create = input("would you like to login, say (yes) for login, or (no) for creating a acoount")
+if login_or_create.lower() == "yes" or "y":
+    login_account()
+else:
+    create_account()
 
-print('Welcome to my shop')
+print (pfg.figlet_format("shopping list",font="larry3d"))
 
-while True:
-    print('Please select what you would like to buy')
-    items_to_buy = start_shop()
 
-    # blah blah 
+print('Please select what you would like to buy')
+items_to_buy = start_shop()
+
+print ("enter contact details:")
+getContactDetails()
+
+print("enter the following infromation:")
+address, canDeliver = get_address()
+
+print ("your shipping cost is:")
+
+postcode = address[2]
+validateStatus(postcode)
+shippingCost = shippingCost(postcode)
+
+total = items_to_buy.get("total_cost") + shippingCost
+print(total)
+
+print("enter payment information:")
+payment_method()
+
+change_currency = input("would you like to switch currencies from GBP? yes or no")
+if change_currency.lower() == "yes" or "y":
+    currency = input("enter the new currency that you would like to change to ")
+    if check_currency_exists(currency):
+        print(f"You will be given {currency_convert("GBP", currency, total)} {currency}")
+
+
+
+
+
+
+
+
+
+
+
+
+
